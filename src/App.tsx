fac258db33fb3a3fe0dee0e6255b2beec6c4f07d
Annabel Allen <annabelja17@gmail.com>
2025-02-02 20:22:44 -0800

    add contact links and clean up css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { ContactLinks } from './components/ContactLinks'
 
 function App() {
   return (
     <>
       <h1>Annabel Allen</h1>
       <h2>Website coming soon...</h2>
+      <footer>
+        <ContactLinks/>
+      </footer>
+      
     </>
   )
 }
---
+++ src/components/ContactLinks.tsx
@@ -0,0 +1,15 @@
+export const ContactLinks = () => {
+    return (
+        <>
+            <a href="mailto:annabelja17@gmail.com" style={{paddingLeft: '4px', paddingRight: '4px'}}>
+                Email
+            </a>
+            <a href="https://www.linkedin.com/in/annabeljallen" style={{paddingLeft: '4px', paddingRight: '4px'}}>
+                LinkedIn
+            </a>
+            <a href="https://github.com/annabelja" style={{paddingLeft: '4px', paddingRight: '4px'}}>
+                Github
+            </a>
+        </>
+    )
+}
