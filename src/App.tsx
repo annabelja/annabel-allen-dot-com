6d84dbe8ce85213204c4639288287dfe1cd3c0b3
Annabel Allen <annabelja17@gmail.com>
2025-06-13 17:14:00 -0700

    add picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { ContactLinks } from './components/ContactLinks'
+import annabelInSeattleImage from './images/Annabel_in_Seattle.jpg'
 
 function App() {
   return (
     <>
       <h1 style={{color: '#002400'}}>Annabel Allen</h1>
+      <img src={annabelInSeattleImage} height={'200px'} style={{borderRadius: '25px'}}/>
       <footer>
         <ContactLinks/>
       </footer>
