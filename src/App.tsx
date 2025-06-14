449b9433d5b50cceb26d88b70ee5148e7d976779
Annabel Allen <annabelja17@gmail.com>
2025-06-13 17:39:53 -0700

    add todo comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <div>
       <h1 style={{color: '#002400'}}>Annabel Allen</h1>
+      {/*TODO: Move styles to css file */}
       <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
       <img src={annabelInSeattleImage} style={{borderRadius: '25px', maxWidth: '200px', padding: '25px', marginLeft: 'auto', marginRight: 'auto'}}/>
       <p style={{alignContent: 'center', minWidth: '200px', marginLeft: 'auto', marginRight: 'auto', padding: '25px'}}>Hi! I&apos;m a Senior Software Engineer at Target, based in Seattle, WA.
+        {/*TODO: implement About section to link to here */}
         {/* <a>
         Click here to learn more about me!
         </a> */}
